082a8d25ac454c79afce1bdd8c0a299038192931
Paula Deville <paula.deville@posteo.net>
2020-09-22 17:04:41 +0200

    Boucles for of

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//***** AFFICHAGE DES PRODUITS SUR LA HOME *****/
+
 //Récupération de la liste des produits
 function getProducts () {
     let url = "http://localhost:3000/api/cameras";
@@ -12,20 +14,16 @@ function getProducts () {
     .then(products => {
         console.log(products);
         //Personnalisation des cards de la home pour chaque produit du tableau
-        for (let p = 0; p < products.length; p += 1) {
-            document.querySelector(".data-img" + p).setAttribute("src", products[p].imageUrl);
-            document.querySelector(".data-name" + p).textContent = products[p].name;
-            document.querySelector(".data-description" + p).textContent = products[p].description;
-            document.querySelector(".data-price" + p).textContent = `${products[p].price / 100} €`;
+        for (let i = 0; i < products.length; i += 1) {
+            document.querySelector(".data-img" + i).setAttribute("src", products[i].imageUrl);
+            document.querySelector(".data-name" + i).textContent = products[i].name;
+            document.querySelector(".data-description" + i).textContent = products[i].description;
+            document.querySelector(".data-price" + i).textContent = `${products[i].price / 100} €`;
             //Transmission de l'ID produit avec l'URL cliquée
-            document.querySelector(".data-link" + p).setAttribute("href", `produit.html?id=${products[p]._id}`);
+            document.querySelector(".data-link" + i).setAttribute("href", `produit.html?id=${products[i]._id}`);
         }
     })
     .catch(error => console.log(error));
 };
+//Appel de la fonction de récupération
 getProducts ();
-
-//Ajout d'un objet local pour remplir le panier pendant la visite du site
-let panier = JSON.parse(localStorage.getItem("panier"));
-console.log(localStorage);
-
--- js/produit.js
+++ js/produit.js
@@ -1,3 +1,10 @@
+//*****STOCKAGE DES DONNEES D'ACHAT *****/
+
+//Ajout d'un objet local pour remplir le panier pendant la visite du site
+let panier = JSON.parse(localStorage.getItem("panier"));
+// console.log(localStorage);
+
+
 //*****PERSONNALISATION DE LA PAGE PRODUIT******
 
 //Création de la fonction de personnalisation
@@ -12,22 +19,23 @@ function viewProduct () {
     fetch(url, options)
     .then(response => response.json())
     .then(products => {
-        for (let i = 0; i < products.length; i += 1) {
+        for (let product of products) {
             //Récupération de l'URL contenant l'ID du produit
-            if (window.location.href.includes(products[i]._id)) {
+            console.log(product);
+            if (window.location.href.includes(product._id)) {
                 //Affichage des données pour chaque produit
-                document.getElementById("titre-produit").textContent = products[i].name;
-                document.getElementById("prix-produit").textContent = `${products[i].price / 100} €`;
-                document.getElementById("img-produit").setAttribute("src", products[i].imageUrl);
-                document.getElementById("description-produit").textContent = products[i].description;
+                document.getElementById("titre-produit").textContent = product.name;
+                document.getElementById("prix-produit").textContent = `${product.price / 100} €`;
+                document.getElementById("img-produit").setAttribute("src", product.imageUrl);
+                document.getElementById("description-produit").textContent = product.description;
                 //Apparition des options dans le bon nombre de cases
-                for (let opt = 0; opt < products[i].lenses.length; opt += 1) {
+                for (let opt = 0; opt < product.lenses.length; opt += 1) {
                     document.querySelector(".data-option" + opt).classList.replace("d-none", "d-block");
-                    document.querySelector(".data-option" + opt).textContent = products[i].lenses[opt];
+                    document.querySelector(".data-option" + opt).textContent = product.lenses[opt];
                 }
                 //Préparation du bouton "panier" en prévision de l'achat
                 document.getElementById("ajout-panier").addEventListener("click", function() {
-                    localStorage.setItem("ajoutPanier", products[i]._id);
+                    localStorage.setItem("ajoutPanier", products._id);
                 });
             };
         }
@@ -36,6 +44,3 @@ function viewProduct () {
 };
 //Appel de la fonction de personnalisation
 viewProduct ();
-
-
-
