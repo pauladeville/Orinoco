657b8e78f1eec798eba1cc1da9dce470cd22245f
Paula Deville <paula.deville@posteo.net>
2020-09-26 17:48:17 +0200

    Simpler form validation

--- js/panier.js
+++ js/panier.js
@@ -85,118 +85,32 @@ document.getElementById("vider-panier").addEventListener("click", function() {
 //Instruction des regExp
 let checkString = /[a-zA-Z]/;
 let checkNumber = /[0-9]/;
-let checkMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/y;
+let checkMail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/
 let checkSpecialCharacter = /[§!@#$%^&*();.?":{}|<>]/;
 
-//Déclaration des fonctions de test
-const validName = function(inputName) {
-    //Récupération de la balise small pour afficher des indications sur le format attendu
-    let indication = inputName.nextElementSibling;
-    if (checkNumber.test(inputName.value) == true || checkSpecialCharacter.test(inputName.value) == true) {
-        indication.classList.add("d-block");
-        indication.textContent = "Ne peut pas contenir des chiffres ou des caractères spéciaux";
-        indication.style.color = "#DC3545";
-    } else {
-        indication.classList.replace("d-block", "d-none");
-        return true;
-    }
-}
-//Test de l'email
-const validEmail = function(inputEmail) {
-    //Récupération de la balise small et personnalisation des messages
-    let indication = inputEmail.nextElementSibling;
-    if (checkMail.test(inputEmail.value) == false) {
-        indication.classList.add("d-block");
-        indication.textContent = "Merci d'indiquer une adresse email valide";
-        indication.style.color = "#DC3545";
-    } else {
-        indication.classList.replace("d-block", "d-none");
-        return true;
-    }
-}
-//Test de l'adresse
-const validAddress = function(inputAddress) {
-    //Récupération de la balise small pour afficher des indications sur le format attendu
-    let msg;
-    let valid = false;
-    if (inputAddress.value.length < 5) {
-        msg = "L'adresse doit contenir au moins 5 caractères";
-    } else if (checkNumber.test(inputAddress.value) == false) {
-        msg = "Vous n'avez pas précisé le numéro de votre adresse";
-    } else if (checkSpecialCharacter.test(inputAddress.value) == true) {
-        msg = "L'adresse ne doit pas comporter de caractères spéciaux";
-    } else {
-        valid = true;
-    }
-    let indication = inputAddress.nextElementSibling;
-    if (!valid) {
-        indication.classList.add("d-block");
-        indication.textContent = msg;
-        indication.style.color = "#DC3545";
-    } else {
-        indication.classList.replace("d-block", "d-none");
-        return true;
-    }
-}
-//Test du code postal
-const validCp = function(inputCp) {
-    let indication = inputCp.nextElementSibling;
-    if (inputCp.value.length !== 5 || checkNumber.test(inputCp.value) == false) {
-        indication.classList.add("d-block");
-        indication.textContent = "Votre code postal doit comporter 5 chiffres";
-        indication.style.color = "#DC3545";
-    } else {
-        indication.classList.replace("d-block", "d-none");
-    }
-}
-//Test de la ville (au moins une option sélectionnée)
-const validCity = function(inputCity) {
-    let indication = inputCity.nextElementSibling;
-    if (inputCity.value.length = 0) {
-        indication.classList.add("d-block");
-        indication.textContent = "Vous devez sélectionner une ville";
-        indication.style.color = "#DC3545";
-    } else {
-        indication.classList.replace("d-block", "d-none");
-        return true;
-    }
-}
+let inputFirstName = document.getElementById("first-name");
+let inputSurname = document.getElementById("surname");
+let inputEmail = document.getElementById("email");
+let inputAddress = document.getElementById("address");
+let inputCp = document.getElementById("cp");
+let inputCity = document.getElementById("city");
 
-//Appel des fonctions de test
-document.getElementById("firstName").addEventListener("change", function() {
-    validName(this);
-})
-document.getElementById("lastName").addEventListener("change", function() {
-    validName(this);
-})
-document.querySelector("#email").addEventListener("change", function() {
-    validEmail(this);
-})
-document.getElementById("address").addEventListener("change", function() {
-    validAddress(this);
-})
-const inputCode = document.getElementById("cp");
-inputCode.addEventListener('change', function() {
-    validCp(this) });
 //Remplissage automatique du champ city à partir du code postal
-inputCode.addEventListener('change', function() {
+inputCp.addEventListener("change", function() {    
     //Requête auprès d'une API publique associant cp/communes
-    let url = `https://geo.api.gouv.fr/communes?codePostal=${inputCode.value}&fields=nom&format=json&geometry=centre`;
+    let url = `https://geo.api.gouv.fr/communes?codePostal=${inputCp.value}&fields=nom&format=json&geometry=centre`;
     fetch(url)
         .then( response => response.json())
         .then( cities => {
-            let indication = inputCode.nextElementSibling;
             //Apparition du bon nombre d'options dans le champ "sville" + placement du nom des villes récupérées 
             if (cities.length == 0) {
-                indication.classList.add("d-block");
-                indication.textContent = "Nous ne connaissons pas ce code postal.";
-                indication.style.color = "#DC3545";
+                document.getElementById("invalid-cp").textContent = "Nous ne connaissons pas ce code postal. Vérifiez qu'il est correctement renseigné.";
             } else {
-                indication.classList.replace("d-block", "d-none");
-                for (let x = 0; x < cities.length; x += 1) {
-                    document.querySelector(".data-option" + x).classList.replace("d-none", "d-block");
-                    document.querySelector(".data-option" + x).textContent = cities[x].nom;
-                    document.querySelector(".data-option" + x).setAttribute("value", cities[x].nom);
+                document.getElementById("invalid-cp").textContent = "";
+                for (let i = 0; i < cities.length; i += 1) {
+                    document.querySelector(".data-option" + i).classList.replace("d-none", "d-block");
+                    document.querySelector(".data-option" + i).textContent = cities[i].nom;
+                    document.querySelector(".data-option" + i).setAttribute("value", cities[i].nom);
                 }
             }
         })
@@ -204,23 +118,101 @@ inputCode.addEventListener('change', function() {
             console.log("Erreur :" + err)
         )
 });
-document.getElementById("city").addEventListener("change", function() {
-    validCity(this);
-})
 
-//Ecouter la soumission du formulaire
+//Validation du champs du formulaire
+checkInput = () => {
+    //Validation du prénom
+    if (checkNumber.test(inputFirstName.value) == true || checkSpecialCharacter.test(inputFirstName.value) == true || inputFirstName.value == "") {
+        document.getElementById("invalid-first-name").textContent = "Votre prénom ne doit pas comporter de chiffre ou de caractères spéciaux";
+        return false;
+    } else {
+        document.getElementById("invalid-first-name").textContent = "";
+    }
+    //Validation du nom
+    if (checkNumber.test(inputSurname.value) == true || checkSpecialCharacter.test(inputSurname.value) == true || inputSurname.value == "") {
+        document.getElementById("invalid-surname").textContent = "Votre nom ne doit pas comporter de chiffre ou de caractères spéciaux";
+        return false;
+    } else {
+        document.getElementById("invalid-surname").textContent = "";
+    }
+    //Validation de l'email
+    if (checkMail.test(inputEmail.value) == false || inputEmail.value == "") {
+        document.getElementById("invalid-email").textContent = "Merci de renseigner une adresse email valide";
+        return false;
+    } else {
+        document.getElementById("invalid-email").textContent = "";
+    }
+    //Validation de l'adresse
+    if (checkString.test(inputAddress.value) == false ||checkNumber.test(inputAddress.value) == false || checkSpecialCharacter.test(inputAddress.value) == true || inputAddress.value == "") {
+        document.getElementById("invalid-address").textContent = "Veuillez vérifier votre adresse";
+        return false;
+    } else {
+        document.getElementById("invalid-address").textContent = "";
+    }
+    //Validation de la ville
+    if (inputCity.value == "") {
+        document.getElementById("invalid-city").textContent = "Renseignez votre code postal et sélectionnez au moins une ville";
+        return false;
+    } else {
+        document.getElementById("invalid-city").textContent = "";
+    }
+}
+
+//Vérification du panier
+checkBasket = () => {
+    let checkBasket = localStorage.getItem("basketList");
+    checkBasket = JSON.parse(checkBasket);
+    if (checkBasket === null) {
+        alert("Vous n'avez aucun article dans votre panier");
+    } else if (checkBasket.length == 0) {
+        alert("Vous n'avez aucun article dans votre panier");
+    } else {
+        return true;
+    }    
+}
+
+let contactUser;
+let productsOrdered = [];
+
+//Création de l'objet à envoyer au clic sur le formulaire
 document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
-    let indications = document.querySelectorAll("small");
-    if (
-        validName(document.getElementById("firstName")) 
-        && validName(document.getElementById("lastName"))
-        // && validEmail(document.getElementById("email"))
-        && validAddress(document.getElementById("address"))
-        && validCity(document.getElementById("city"))
-    ) {
-        form.submit();
-    } else {
-        alert("Vous n'avez pas terminé de remplir le formulaire :)");
+    //Si formulaire et panier OK, création de l'objet "coordonnées" du client
+    if (checkInput() !== false && checkBasket() == true) {
+        contactUser = {
+            firstName: inputFirstName.value,
+            lastName: inputSurname.value,
+            address: inputAddress.value,
+            city: inputCity.value,
+            email: inputEmail.value,
+        };
+        //Récupération des ID des produits du panier
+        let basketFinal = localStorage.getItem("basketList");
+        basketFinal = JSON.parse(basketFinal);
+        for (let product of basketFinal) {
+            productsOrdered.push(product._id)
+        }
+        //Ajout des éléments dans l'objet destiné à l'API
+        let dataToSend = {contactUser, productsOrdered};
+        console.log(dataToSend);
+        sendOrder(dataToSend);
     }
-}); 
+});
+
+//Déclaration de la fonction d'envoi
+sendOrder = (objectRequest) => {
+    let url = "http://localhost:3000/api/cameras/order";
+    let options = {
+        method: "POST",
+        body: JSON.stringify(objectRequest),
+        headers: {
+            "Content-type": "application/JSON"
+        },
+    }
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log(error));
+};
